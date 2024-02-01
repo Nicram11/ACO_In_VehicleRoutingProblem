10bff99a76cad9c8ce57534b11d3ddc31ecb6364
Nicram11 <mar.roszyk@gmail.com>
2024-02-01 21:38:44 +0100

    change capacity

--- src/Env.java
+++ src/Env.java
@@ -8,13 +8,10 @@ public class Env {
     public static double beta = 2.0;
     public static double initialPheromoneLevel = 1.0;
     public static int numberOfAnts = 50;
-    public static int capacity = 1000;
-
-
+    public static int capacity = 1200;
 
     public static double mutationProbability = 0.5;
     public static int hilltopSearchTryCount = 100;
     public static int findingMutationTryCount = 3;
-
 
 }
